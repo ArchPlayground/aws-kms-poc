d8dfd66d2560ad5bab7747729ab80233fdcd4980
RahulV <rverma@upchain.com>
2020-07-14 16:23:28 -0400

    :sparkles: Adds Spring boot base app

--- using-cli/decrypt.sh
+++ using-cli/decrypt.sh
@@ -3,7 +3,6 @@ source ./kms_constants.sh
 echo "Using Key ID: $KEY_ID"
 echo "Using Encryption context: $ENCRYPTION_CONTEXT"
 
-
 echo "Decrypting contents of : $OUTPUT_BLOB"
 
 aws kms decrypt --ciphertext-blob fileb://$OUTPUT_BLOB --encryption-context $ENCRYPTION_CONTEXT --query Plaintext --output text | base64 --decode
--- using-cli/encrypt.sh
+++ using-cli/encrypt.sh
@@ -3,10 +3,9 @@ source ./kms_constants.sh
 echo "Using Key ID: $KEY_ID"
 echo "Using Encryption context: $ENCRYPTION_CONTEXT"
 
-
 echo "Encrypting : $SECRET_BLOB"
 
-aws kms encrypt --key-id $KEY_ID --plaintext $SECRET_BLOB --encryption-context $ENCRYPTION_CONTEXT --output text --query CiphertextBlob | base64 --decode > $OUTPUT_BLOB
+aws kms encrypt --key-id $KEY_ID --plaintext $SECRET_BLOB --encryption-context $ENCRYPTION_CONTEXT --output text --query CiphertextBlob | base64 --decode >$OUTPUT_BLOB
 
 echo "Encryption complete. Encrypted output stored in $OUTPUT_BLOB"
 
